c93e61ad3a154cb00dce4a72abfbaa15fcd21dac
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-04-29 14:37:58 +0200

    RWD for Phones

--- src/components/Phones/StyledPhone.tsx
+++ src/components/Phones/StyledPhone.tsx
@@ -9,14 +9,27 @@ export const StyledPhones = styled.section`
   padding: 0 165px;
   border-radius: 0px 100px;
   background: linear-gradient(135deg, #2d2e40 0%, #3f4164 100%);
+  @media (min-width: ${({ theme }) => theme.breakpoints.hugeScreens}) {
+    margin-bottom: 400px;
+  }
   @media (${({ theme }) => theme.breakpoints.tabletDesign}) {
+    position: relative;
     flex-direction: column;
     align-items: center;
     gap: 0;
-    height: 625px;
     width: 100%;
+    height: 490px;
     margin-top: 600px;
     padding: 0;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.maxMobileDesign}) {
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    gap: 46px;
+    height: 625px;
+    padding: 0 24px 109px 24px;
+    margin-top: 200px;
   }
 `;
 
@@ -29,32 +42,30 @@ export const StyledImg = styled.div`
     height: 799px;
   }
   @media (${({ theme }) => theme.breakpoints.tabletDesign}) {
+    position: absolute;
+    top: -42px;
+    right: 50%;
+    left: 50%;
+    transform: translateX(-50%);
     width: 328px;
     height: 347px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.maxMobileDesign}) {
+    position: absolute;
+    top: 0;
+    right: 50%;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 347px;
+    width: 328px;
   }
   img {
     position: absolute;
     bottom: 57px;
-    @media (${({ theme }) => theme.breakpoints.tabletDesign}) {
-      left: 50%;
-      right: 50%;
-      transform: translateX(-50%);
-      bottom: 92px;
-    }
     @media (min-width: ${({ theme }) => theme.breakpoints.hugeScreens}) {
       bottom: 87px;
     }
   }
-  /* ::before {
-    content: url("bg-pattern-circles.svg");
-    display: block;
-    position: absolute;
-    width: 1014px;
-    height: 1018px;
-    top: 0;
-    left: 0;
-    z-index: 1;
-  } */
 `;
 
 export const StyledTextWrapper = styled.div`
@@ -66,10 +77,14 @@ export const StyledTextWrapper = styled.div`
   background-color: transparent;
   color: var(--white);
   @media (${({ theme }) => theme.breakpoints.tabletDesign}) {
-    align-items: center;
+    padding-top: 190px;
   }
   @media (min-width: ${({ theme }) => theme.breakpoints.hugeScreens}) {
     width: 840px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.maxMobileDesign}) {
+    width: 327px;
+    padding-top: 320px;
   }
 `;
 
@@ -83,6 +98,9 @@ export const StyledTitle = styled.h2`
     line-height: 61px;
     letter-spacing: -1.2px;
   }
+  @media (max-width: ${({ theme }) => theme.breakpoints.maxMobileDesign}) {
+    text-align: center;
+  }
 `;
 
 export const StyledDescription = styled.p`
@@ -90,6 +108,9 @@ export const StyledDescription = styled.p`
   line-height: 28px;
   letter-spacing: 0.5px;
   @media (${({ theme }) => theme.breakpoints.tabletDesign}) {
+    text-align: center;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.maxMobileDesign}) {
     text-align: center;
   }
 `;
